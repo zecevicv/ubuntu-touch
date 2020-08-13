788c451ed9301b37ea3a6a84f3f1be1d709436d8
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2020-08-13 11:33:53 +0200

    [ADD] - Header backdrop

--- js/main.js
+++ js/main.js
@@ -3,17 +3,13 @@ window.addEventListener('load', () => {
   ======================================================= */
   const body = document.querySelector('body');
   body.addEventListener('click', (e) => {
-    if (!e.target.closest('.header') && window.innerWidth > 992) {
+    if ((e.target.closest('.header-backdrop') || !e.target.closest('.header-dropdown')) && window.innerWidth > 992) {
       $('#aboutUsCollapse').collapse('hide');
       $('#contributorsCollapse').collapse('hide');
       $('#communityCollapse').collapse('hide');
       $('#getUbuntuTouchCollapse').collapse('hide');
-    } else if (!e.target.closest('.header') && window.innerWidth < 992) {
+    } else if (e.target.closest('.header-backdrop') && window.innerWidth < 992) {
       $('#headerNav').collapse('hide');
     }
   });
-
-  /* #Adding backdrop on mega-menu
-  ======================================================= */
-
 });
